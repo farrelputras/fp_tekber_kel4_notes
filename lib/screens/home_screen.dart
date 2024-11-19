6555f9f293d29a74b269bcb53f0e9b01cbf37302
Khalid Wildan Lazuardi <khalidlazuardi52@gmail.com>
2024-11-19 16:38:09 +0700

    olid nyoba dengan teman

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:fp_tekber_kel4_notes/widget/notes_body.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -7,15 +6,38 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        backgroundColor: const Color.fromARGB(255, 255, 243, 132), // Warna kuning untuk AppBar
+        centerTitle: true, // Teks judul di tengah
+        title: const Text(
+          'Notes',
+          style: TextStyle(
+            color: Colors.black, // Teks warna hitam
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
       floatingActionButton: FloatingActionButton(
-          backgroundColor: Colors.blue[50],
-          elevation: 0,
-          onPressed: () {},
-          child: Icon(
-            Icons.add,
-            color: Colors.black,
-          )),
-      body: NotesBody(),
+        backgroundColor: Colors.yellow, // Warna kuning untuk FAB
+        elevation: 0, // Hilangkan bayangan FAB
+        onPressed: () {
+          // Tambahkan aksi FAB
+        },
+        child: const Icon(
+          Icons.add,
+          color: Colors.black, // Warna ikon hitam
+        ),
+      ),
+      body: const Center(
+        child: Text(
+          'No notes yet!',
+          style: TextStyle(
+            fontSize: 18,
+            color: Colors.grey, // Warna teks placeholder
+          ),
+        ),
+      ),
     );
   }
 }

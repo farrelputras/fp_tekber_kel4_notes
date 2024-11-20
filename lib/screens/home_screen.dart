f32fc7b1b0e2f1f35772ff112690a27cf22b3679
Fernandio Farrel Putra Susilo <113741206+farrelputras@users.noreply.github.com>
2024-11-20 19:05:53 +0700

    navbar - onyx

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,17 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:fp_tekber_kel4_notes/screens/add_note_screen.dart';
+import 'package:get/get.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final controller = Get.put(NavController());
+
     return Scaffold(
+      //Start AppBar
       appBar: AppBar(
-        backgroundColor: const Color.fromARGB(255, 255, 243, 132), // Warna kuning untuk AppBar
+        backgroundColor: const Color.fromARGB(
+            255, 255, 243, 132), // Warna kuning untuk AppBar
         centerTitle: true, // Teks judul di tengah
         title: const Text(
-          'Notes',
+          'EduNotes',
           style: TextStyle(
             color: Colors.black, // Teks warna hitam
             fontSize: 20,
@@ -19,22 +24,35 @@ class HomeScreen extends StatelessWidget {
           ),
         ),
       ),
+      //Start NavBar
+      bottomNavigationBar: Obx(() => NavigationBar(
+            selectedIndex: controller.selectedIndex.value,
+            onDestinationSelected: (index) =>
+                controller.selectedIndex.value = index,
+            destinations: const [
+              NavigationDestination(icon: Icon(Icons.note_alt), label: 'Notes'),
+              NavigationDestination(
+                  icon: Icon(Icons.check_box_outlined), label: 'ToDoList')
+            ],
+          )),
+
+      //Start ActionButton
       floatingActionButton: FloatingActionButton(
-  backgroundColor: const Color.fromARGB(255, 189, 117, 202),
-  elevation: 0,
-  onPressed: () {
-    Navigator.push(
-      context,
-      MaterialPageRoute(
-        builder: (context) => const AddNoteScreen(),
+        backgroundColor: const Color.fromARGB(255, 189, 117, 202),
+        elevation: 0,
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => const AddNoteScreen(),
+            ),
+          );
+        },
+        child: const Icon(
+          Icons.add,
+          color: Colors.black,
+        ),
       ),
-    );
-  },
-  child: const Icon(
-    Icons.add,
-    color: Colors.black,
-  ),
-),
       body: const Center(
         child: Text(
           'No notes yet!',
@@ -46,4 +64,8 @@ class HomeScreen extends StatelessWidget {
       ),
     );
   }
+}
+
+class NavController extends GetxController {
+  final Rx<int> selectedIndex = 0.obs;
 }
--- lib/widget/custIcon.dart
+++ lib/widget/custIcon.dart
@@ -1,25 +1,25 @@
-import 'package:flutter/material.dart';
+// import 'package:flutter/material.dart';
 
-class CustIcon extends StatelessWidget {
-  const CustIcon({super.key, required this.icon, this.onPressed});
-  final IconData icon;
-  final void Function()? onPressed;
+// class CustIcon extends StatelessWidget {
+//   const CustIcon({super.key, required this.icon, this.onPressed});
+//   final IconData icon;
+//   final void Function()? onPressed;
 
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      width: 45,
-      height: 45,
-      decoration: BoxDecoration(
-          color: Colors.grey.withOpacity(0.1),
-          borderRadius: BorderRadius.circular(16)),
-      child: IconButton(
-          onPressed: onPressed,
-          icon: Icon(
-            icon,
-            size: 28,
-            color: Colors.black,
-          )),
-    );
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     return Container(
+//       width: 45,
+//       height: 45,
+//       decoration: BoxDecoration(
+//           color: Colors.grey.withOpacity(0.1),
+//           borderRadius: BorderRadius.circular(16)),
+//       child: IconButton(
+//           onPressed: onPressed,
+//           icon: Icon(
+//             icon,
+//             size: 28,
+//             color: Colors.black,
+//           )),
+//     );
+//   }
+// }
